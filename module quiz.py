f7ec6ae951a37994883d909d16bc55aebfc39eb7
ChesangFelister <47848763+ChesangFelister@users.noreply.github.com>
2019-05-09 19:03:59 +0300

    Add files via upload
    class test in function and lists and dictionaries

---
+++ module quiz.py
@@ -0,0 +1,52 @@
+def list():
+	g=[ for number in range(100,150),if number%3==0:, h[y]=g%3]
+		print(h)
+
+def sorted():
+	a=["apple","banana","mango"]
+	b=["avocado","peach","orange"]
+	c=["pineapple","lemon"]
+	d=a+b+c
+	d.sort()
+	print(d)
+
+def divisible_by_three():
+	g=range(1,10)
+	for n in g:
+		if n%3==0
+		print(n)
+
+def lists():
+	x=[[1,2],[3,4],[5,6]]
+	g=x.set()
+	print(g)
+
+def smallest():
+	a=[1,3,6,8,2,4,5.7]
+	print(min(a))
+
+def remove():
+	x=["a","b","a","e","d","b","c","e","f","g","h"]
+	x.remove(b)
+	x.append(a)
+	x.remove(e)
+	print(x)
+
+
+
+def squares():
+	k=dict()
+	for x in range(149,159)
+	j[k]=x*x
+	print(j)
+
+def student():
+	student1 = {"age": 19, "name": "Eunice"}
+	student2={"age": 21, "name": "Agnes"}
+	student3={"age": 18, "name": "Teresa"}
+	student4={"age":22,"name":"Asha"}
+	students=[student1,student2,student3,student4]
+	year=2019-(student("age"))
+	print("Hello {} your born in the year{}.".format(student["age"],student["name"]))
+
+
